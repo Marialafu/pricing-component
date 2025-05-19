19dc26ee4645f84b6ff78649654d656f1df2a6d6
Marialafu <lafuentelhmaria@gmail.com>
2025-05-19 10:04:55 +0200

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,9 @@ import { useState } from 'react';
 import PricingSwitch from './components/pricingSwitch/PricingSwitch';
 import { GlobalStyles } from './styles/GlobalStyles';
 import Cards from './components/cards/Cards';
-import { StyledTitle } from './components/cards/CardsStyles';
 import Title from './components/title/Title';
 
-
 const App = () => {
-
   const [anually, setAnually] = useState(true);
 
   return (
@@ -20,7 +17,6 @@ const App = () => {
         <PricingSwitch setAnually={setAnually} anually={anually} />
         <Cards anually={anually} />
       </main>
-
     </>
   );
 };
--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -1,49 +1,48 @@
-import { v4 } from 'uuid'
-import {MONTHLY_INFO} from '../../constants/MonthlyInfo'
-import {ANUALLY_INFO} from '../../constants/AnuallyInfo'
+import { v4 } from 'uuid';
+import { MONTHLY_INFO } from '../../constants/MonthlyInfo';
+import { ANUALLY_INFO } from '../../constants/AnuallyInfo';
 
-import { CardContainer, CardsContainer, RemarkedButton, RemarkedCardContainer, RemarkedSubtext, RemarkedText, RemarkedTitle, StyledButton, StyledSubtext, StyledText, StyledTitle } from './CardsStyles'
-import { useState } from 'react'
+import {
+  CardContainer,
+  CardsContainer,
+  StyledButton,
+  StyledSubtext,
+  StyledText,
+  StyledTitle
+} from './CardsStyles';
 
-const Cards = ({anually}) => {
+const Cards = ({ anually }) => {
+  const cardsValues = defineInfoValues(anually);
 
-    const cardsValues = defineInfoValues(anually)
+  return (
+    <CardsContainer>
+      {cardsValues.map(card => {
+        return (
+          <CardContainer key={v4()} plan={card.title}>
+            <StyledTitle plan={card.title}>{card.title}</StyledTitle>
+            <StyledText plan={card.title}>$ {card.price}</StyledText>
+            <StyledSubtext plan={card.title}>
+              {card.storage} Storage
+            </StyledSubtext>
+            <StyledSubtext plan={card.title}>
+              {card.users} Users Allowed
+            </StyledSubtext>
+            <StyledSubtext plan={card.title}>
+              Send up to {card.send} GB
+            </StyledSubtext>
+            <StyledButton plan={card.title}>LEARN MORE</StyledButton>
+          </CardContainer>
+        );
+      })}
+    </CardsContainer>
+  );
+};
 
-    return (
-        <CardsContainer>
+const defineInfoValues = anually => {
+  const defineValues = anually ? ANUALLY_INFO : MONTHLY_INFO;
+  console.log(defineValues);
 
-        {cardsValues.map(card => 
+  return defineValues;
+};
 
-        {if (card.title !== 'Professional'){
-            return <CardContainer key={v4()}>
-                <StyledTitle>{card.title}</StyledTitle>
-                <StyledText>$ {card.price}</StyledText>
-                <StyledSubtext>{card.storage} Storage</StyledSubtext>
-                <StyledSubtext>{card.users} Users Allowed</StyledSubtext>
-                <StyledSubtext>Send up to {card.send} GB</StyledSubtext>
-                <StyledButton>LEARN MORE</StyledButton>
-            </CardContainer>
-        } else {
-            return <RemarkedCardContainer key={v4()}>
-                <RemarkedTitle>{card.title}</RemarkedTitle>
-                <RemarkedText>$ {card.price}</RemarkedText>
-                <RemarkedSubtext>{card.storage} Storage</RemarkedSubtext>
-                <RemarkedSubtext>{card.users} Users Allowed</RemarkedSubtext>
-                <RemarkedSubtext>Send up to {card.send} GB</RemarkedSubtext>
-                <RemarkedButton>LEARN MORE</RemarkedButton>
-            </RemarkedCardContainer>
-        }}
-        )}
-        </CardsContainer>
-    )
-
-}
-
-const defineInfoValues = (anually) => {
-    const defineValues =  anually ? ANUALLY_INFO : MONTHLY_INFO
-    console.log(defineValues);
-    
-    return defineValues
-}
-
-export default Cards
+export default Cards;
